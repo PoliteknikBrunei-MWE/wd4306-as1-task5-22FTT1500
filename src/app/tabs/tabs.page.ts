0478af0b5d892aba0d51b2ad5c2018db9e69365b
22FTT1500 <22FTT1500@student.pb.edu.bn>
2024-03-24 22:37:26 +0800

    add tabs buttons on tabs page

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
-
+import { addIcons } from 'ionicons';
+import { triangle, ellipse, square } from 'ionicons/icons';
 @Component({
   selector: 'app-tabs',
   templateUrl: './tabs.page.html',
@@ -12,7 +13,9 @@ import { IonicModule } from '@ionic/angular';
 })
 export class TabsPage implements OnInit {
 
-  constructor() { }
+  constructor() { 
+    addIcons({ triangle, ellipse, square });
+  }
 
   ngOnInit() {
   }
